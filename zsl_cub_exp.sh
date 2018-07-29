bbfe93c9fb0b6a90ff2cfc9fd52245d334fe8bf7
Yizhe (Ethan) Zhu <14117987+EthanZhu90@users.noreply.github.com>
2018-07-28 17:10:06 -0700

    Update zsl_cub_exp.sh

--- zsl_cub_exp.sh
+++ zsl_cub_exp.sh
@@ -70,3 +70,4 @@ python cls_ABP.py --dataset CUB --latent_dim 5 --sigma 0.3 --langevin_s 0.3 --la
 python cls_ABP.py --dataset CUB --latent_dim 5 --sigma 0.3 --langevin_s 0.3 --langevin_step 50 --preprocessing --cuda --image_embedding res101 --class_embedding att --nepoch 100 --ngh 4096 --ndh 4096 --lr 0.0001 --batch_size 64 --nz 312 --attSize 312 --resSize 2048 --syn_num 300 
 python cls_ABP.py --dataset CUB --latent_dim 5 --sigma 0.3 --langevin_s 0.3 --langevin_step 50 --preprocessing --cuda --image_embedding res101 --class_embedding att --nepoch 100 --ngh 4096 --ndh 4096 --lr 0.0001 --batch_size 64 --nz 312 --attSize 312 --resSize 2048 --syn_num 300 
 
+
